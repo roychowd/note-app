fe7c4c961630c62aecf46086a6ff04615919477b
Roy Chowdhury <chowdhury.roy4@gmail.com>
2018-06-07 16:28:37 -0600

    initial commit

---
+++ notes.js
@@ -0,0 +1,14 @@
+console.log('file is starting notes.js');
+
+module.exports.addNote = () => {
+    console.log('addnote');
+    return 'New note';
+    
+};
+
+module.exports.add = (a,b) => {
+    return a+b;
+};
+
+
+// console.log(module)
